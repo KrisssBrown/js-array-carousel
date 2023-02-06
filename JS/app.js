5391389421b2dba4bf5b6b889bb4dfde17dc5a50
KrisssBrown <cristianbruno.cs@gmail.com>
2023-02-06 17:37:31 +0100

    START

---
+++ JS/app.js
@@ -0,0 +1,58 @@
+let indiceSlideAttiva = 0
+// let clickCounter = 0
+const slideElements = document.getElementsByClassName('slide')
+
+
+const leftBtnEl = document.querySelector('.arrow.carousel-left-arrow')
+const rightBtnEl = document.querySelector('.arrow.carousel-right-arrow')
+
+rightBtnEl.addEventListener('click', function () {
+    
+    console.log('slide corrente', indiceSlideAttiva)
+   
+    let slideCorrente = slideElements[indiceSlideAttiva]
+   //   togliendo la classe active
+    slideCorrente.classList.remove('active')
+
+   //   inremento l'indice
+    indiceSlideAttiva += 1
+
+    let prossimaSlide = slideElements[indiceSlideAttiva]
+   //   aggiungiamo la classe active alla seconda slide
+    prossimaSlide.classList.add('active')
+
+    console.log('next slide', indiceSlideAttiva)
+    
+    if (indiceSlideAttiva === 4){
+        let slideCorrente = slideElements[indiceSlideAttiva]
+   //   togliendo la classe active
+        slideCorrente.classList.remove('active')
+
+   //   inremento l'indice
+        indiceSlideAttiva += 1
+
+        let prossimaSlide = slideElements[indiceSlideAttiva]
+   //   aggiungiamo la classe active alla seconda slide
+        prossimaSlide.classList.add('active')
+
+    console.log('next slide', indiceSlideAttiva)
+    }
+})
+
+
+
+    
+
+    
+    
+    
+        
+    
+
+    
+
+
+    
+    
+
+    
