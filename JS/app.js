536ac51e2f3dc100712aca80ce578a679694d0a4
KrisssBrown <cristianbruno.cs@gmail.com>
2023-02-06 19:01:39 +0100

    FEAT loop added

--- JS/app.js
+++ JS/app.js
@@ -7,6 +7,40 @@ const leftBtnEl = document.querySelector('.arrow.carousel-left-arrow')
 const rightBtnEl = document.querySelector('.arrow.carousel-right-arrow')
 
 rightBtnEl.addEventListener('click', function () {
+    if (indiceSlideAttiva < 6){
+        
+        console.log('slide corrente', indiceSlideAttiva)
+       
+        let slideCorrente = slideElements[indiceSlideAttiva]
+       //   togliendo la classe active
+        slideCorrente.classList.remove('active')
+    
+       //   inremento l'indice
+        indiceSlideAttiva += 1
+    
+        let prossimaSlide = slideElements[indiceSlideAttiva]
+       //   aggiungiamo la classe active alla seconda slide
+        prossimaSlide.classList.add('active')
+    
+        console.log('next slide', indiceSlideAttiva)
+        
+        if (indiceSlideAttiva === 5){
+            let slideCorrente = slideElements[indiceSlideAttiva]
+            slideCorrente.classList.remove('active')
+            
+            indiceSlideAttiva = 0
+    
+            let prossimaSlide = slideElements[indiceSlideAttiva]
+            // aggiungiamo la classe active alla seconda slide
+            prossimaSlide === 0
+            prossimaSlide.classList.add('active')
+            
+        }
+    }
+        
+})
+
+leftBtnEl.addEventListener('click', function () {
     
     console.log('slide corrente', indiceSlideAttiva)
    
@@ -15,28 +49,14 @@ rightBtnEl.addEventListener('click', function () {
     slideCorrente.classList.remove('active')
 
    //   inremento l'indice
-    indiceSlideAttiva += 1
+    indiceSlideAttiva -= 1
 
     let prossimaSlide = slideElements[indiceSlideAttiva]
    //   aggiungiamo la classe active alla seconda slide
     prossimaSlide.classList.add('active')
 
     console.log('next slide', indiceSlideAttiva)
-    
-    if (indiceSlideAttiva === 4){
-        let slideCorrente = slideElements[indiceSlideAttiva]
-   //   togliendo la classe active
-        slideCorrente.classList.remove('active')
 
-   //   inremento l'indice
-        indiceSlideAttiva += 1
-
-        let prossimaSlide = slideElements[indiceSlideAttiva]
-   //   aggiungiamo la classe active alla seconda slide
-        prossimaSlide.classList.add('active')
-
-    console.log('next slide', indiceSlideAttiva)
-    }
 })
 
 
