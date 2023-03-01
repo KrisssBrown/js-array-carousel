3afd994cb25fbeef91c60ca617f0449e49b1903c
KrisssBrown <cristianbruno.cs@gmail.com>
2023-03-01 09:47:38 +0100

    .

--- JS/app.js
+++ JS/app.js
@@ -3,11 +3,14 @@ let indiceSlideAttiva = 0
 const slideElements = document.getElementsByClassName('slide')
 
 
+
 const leftBtnEl = document.querySelector('.arrow.carousel-left-arrow')
 const rightBtnEl = document.querySelector('.arrow.carousel-right-arrow')
 
 rightBtnEl.addEventListener('click', function () {
-    if (indiceSlideAttiva < 6){
+    const lastIndex = indiceSlideAttiva.lenght - 1
+    
+    if (indiceSlideAttiva < lastIndex.lenght){
         
         console.log('slide corrente', indiceSlideAttiva)
        
@@ -24,18 +27,18 @@ rightBtnEl.addEventListener('click', function () {
     
         console.log('next slide', indiceSlideAttiva)
         
-        if (indiceSlideAttiva === 5){
-            let slideCorrente = slideElements[indiceSlideAttiva]
-            slideCorrente.classList.remove('active')
-            
-            indiceSlideAttiva = 0
-    
-            let prossimaSlide = slideElements[indiceSlideAttiva]
-            // aggiungiamo la classe active alla seconda slide
-            prossimaSlide === 0
-            prossimaSlide.classList.add('active')
-            
-        }
+    }
+    else {
+        let slideCorrente = slideElements[indiceSlideAttiva]
+        slideCorrente.classList.remove('active')
+        
+        indiceSlideAttiva = 0
+
+        let prossimaSlide = slideElements[indiceSlideAttiva]
+        // aggiungiamo la classe active alla seconda slide
+        // prossimaSlide === 0
+        prossimaSlide.classList.add('active')
+        
     }
         
 })
